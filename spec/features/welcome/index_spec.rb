4a4452415f2c7f311f8f959713fced0b33a6cf3f
Curtis Bartell <cbartell980@gmail.com>
2021-01-01 15:44:54 -0700

    Add test for user log in and registration buttons on home page

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -2,10 +2,16 @@ require 'rails_helper'
 
 describe 'As a visitor' do
   describe 'when I visit "/"' do
-    it 'I see a welcome page' do
+    before :each do
       visit root_path
-
+    end
+    it 'I see a welcome message' do
       expect(page).to have_content("Welcome to OmniHub!")
+    end
+
+    it 'I see buttons to log in and register with GH OAuth' do
+      expect(page).to have_button('Log In with GitHub Account')
+      expect(page).to have_button('Register With GitHub Account')
     end
   end
 end
