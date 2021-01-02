28c23832e159a428bff228db4e8703115a259841
Curtis Bartell <cbartell980@gmail.com>
2021-01-02 16:16:09 -0700

    Remove redundant test for log in link

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -8,9 +8,5 @@ describe 'As a visitor' do
     it 'I see a welcome message' do
       expect(page).to have_content("Welcome to OmniHub!")
     end
-
-    it 'I see buttons to log in and register with GH OAuth' do
-      expect(page).to have_button('Log In with GitHub Account')
-    end
   end
 end
