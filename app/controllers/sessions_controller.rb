8e2dd25f09725c44542b771353c0c6b857e82577
Curtis Bartell <cbartell980@gmail.com>
2021-01-03 09:57:55 -0700

    Refactor sessions#create with helper methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,36 @@
 class SessionsController < ApplicationController
   def create
+    access_token = get_access_token(params[:code])
+    user_data = get_user_data(access_token)
+    user = User.find_or_create_by(uid: user_data[:id])
+    user.username = user_data[:login]
+    user.uid = user_data[:id]
+    user.token = access_token
+    user.save
+
+    session[:user_id] = user.id
+
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def get_access_token(code)
     conn = Faraday.new(
       url: 'https://github.com',
-      headers: {
-        'Accept': 'application/json'
-      }
+      headers: { 'Accept': 'application/json' }
     )
 
     response = conn.post('/login/oauth/access_token') do |req|
-      req.params['code'] = params[:code]
+      req.params['code'] = code
       req.params['client_id'] = ENV['CLIENT_ID']
       req.params['client_secret'] = ENV['CLIENT_SECRET']
     end
 
-    data = JSON.parse(response.body, symbolize_names: true)
-    access_token = data[:access_token]
+    JSON.parse(response.body, symbolize_names: true)[:access_token]
+  end
 
+  def get_user_data(access_token)
     conn = Faraday.new(
       url: 'https://api.github.com',
       headers: {
@@ -24,16 +39,7 @@ class SessionsController < ApplicationController
     )
 
     response = conn.get('/user')
-    data = JSON.parse(response.body, symbolize_names: true)
 
-    user = User.find_or_create_by(uid: data[:id])
-    user.username = data[:login]
-    user.uid = data[:id]
-    user.token = access_token
-    user.save
-
-    session[:user_id] = user.id
-
-    redirect_to dashboard_path
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
