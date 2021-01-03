cf3f0c2b712725a248980b955f614997004e3cae
Curtis Bartell <cbartell980@gmail.com>
2021-01-02 21:56:05 -0700

    Create sanitized VCR cassettes

--- spec/features/sessions/new_spec.rb
+++ spec/features/sessions/new_spec.rb
@@ -5,7 +5,7 @@ describe 'User login' do
     describe 'When I visit "/" and login through the log in link' do
       it 'I am taken to my dashboard page' do
         VCR.use_cassette('oauth_handshake') do
-          visit gh_callback_path(code: "c7b29b687e04365e79d5")
+          visit gh_callback_path(code: "24d1ed1389d66d469bda")
 
           expect(current_url).to eq(dashboard_path)
           expect(page).to have_content("Welcome c-bartell!")
