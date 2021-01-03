263daa601bd8bdef1814ed72635f21d4c45e5dcb
Curtis Bartell <cbartell980@gmail.com>
2021-01-03 10:16:08 -0700

    Add note about cassette creation

--- spec/features/sessions/new_spec.rb
+++ spec/features/sessions/new_spec.rb
@@ -5,6 +5,9 @@ describe 'User login' do
     describe 'When I visit "/" and login through the log in link' do
       it 'I am taken to my dashboard page' do
         VCR.use_cassette('oauth_handshake') do
+          # Code must be retrieved manually from the 'code' param sent in GH
+          # Oauth login callback. This code is single use and only lasts 10
+          # minutes, so this must be done every time the cassettes are created.
           visit gh_callback_path(code: "24d1ed1389d66d469bda")
 
           expect(current_path).to eq(dashboard_path)
