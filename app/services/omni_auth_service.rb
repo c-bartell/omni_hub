327bf75b6788bee35e4734a1479326eafd045e5c
Curtis Bartell <cbartell980@gmail.com>
2021-01-03 10:42:11 -0700

    Refactor service with class << self block

--- app/services/omni_auth_service.rb
+++ app/services/omni_auth_service.rb
@@ -1,20 +1,23 @@
 class OmniAuthService
-  def self.request_token(code)
-    response = conn.post('/login/oauth/access_token') do |req|
-      req.params['code'] = code
-      req.params['client_id'] = ENV['CLIENT_ID']
-      req.params['client_secret'] = ENV['CLIENT_SECRET']
+  class << self
+    def request_token(code)
+      response = conn.post('/login/oauth/access_token') do |req|
+        req.params['code'] = code
+        req.params['client_id'] = ENV['CLIENT_ID']
+        req.params['client_secret'] = ENV['CLIENT_SECRET']
+      end
+
+      JSON.parse(response.body, symbolize_names: true)
     end
 
-    JSON.parse(response.body, symbolize_names: true)
-  end
+    private
 
-  private
-
-  def self.conn
-    Faraday.new(
-      url: 'https://github.com',
-      headers: { 'Accept': 'application/json' }
-    )
+    def conn
+      private_class_method
+      Faraday.new(
+        url: 'https://github.com',
+        headers: { 'Accept': 'application/json' }
+      )
+    end
   end
 end
