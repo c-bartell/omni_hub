4fdfa41d3346dcdaa9fe510cd9d9c4b4a538c192
Curtis Bartell <cbartell980@gmail.com>
2021-01-02 17:30:17 -0700

    Move log in link test from sessions spec to welcome spec

--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -1,17 +1,5 @@
 require 'rails_helper'
 
-describe 'Sessions' do
-  describe 'As a visitor' do
-    describe 'When I visit the root page' do
-      it 'I see a log in button that takes me to the GitHub OAuth page for my app' do
-        visit root_path
+describe 'User Authentication and Authorization' do
 
-        expect(ENV['CLIENT_ID']).to_not be nil
-
-        link = "https://github.com/login/oauth/authorize?client_id=#{ENV['CLIENT_ID']}&scope=repo"
-
-        expect(page).to have_link('Log In with GitHub Account', href: link)
-      end
-    end
-  end
 end
