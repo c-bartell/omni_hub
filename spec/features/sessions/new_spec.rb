8adc0ab30501249c405f5911025894a70380ae81
Curtis Bartell <cbartell980@gmail.com>
2021-01-02 20:39:46 -0700

    Add test for user login"

--- spec/features/sessions/new_spec.rb
+++ spec/features/sessions/new_spec.rb
@@ -4,12 +4,12 @@ describe 'User login' do
   describe 'As a visitor' do
     describe 'When I visit "/" and login through the log in link' do
       it 'I am taken to my dashboard page' do
-        code = "placeholder"
+        VCR.use_cassette('oauth_handshake') do
+          visit gh_callback_path(code: "c7b29b687e04365e79d5")
 
-        visit gh_callback_path(code: code)
-
-        expect(current_url).to eq(dashboard_path)
-        expect(page).to have_content("Welcome c-bartell!")
+          expect(current_url).to eq(dashboard_path)
+          expect(page).to have_content("Welcome c-bartell!")
+        end
       end
     end
   end
