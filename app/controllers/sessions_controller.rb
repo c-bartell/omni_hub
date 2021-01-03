682fc36d0afbdd8f6cf310c2446b56653ea80781
Curtis Bartell <cbartell980@gmail.com>
2021-01-03 13:21:19 -0700

    Create GitHubService and consume /user endpoint

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     access_token = OmniAuthFacade.access_token(params[:code])
-    user_data = get_user_data(access_token)
+    user_data = GitHubService.user_data(access_token)
     user = User.find_or_create_by(uid: user_data[:id])
     user.username = user_data[:login]
     user.uid = user_data[:id]
@@ -11,20 +11,5 @@ class SessionsController < ApplicationController
     session[:user_id] = user.id
 
     redirect_to dashboard_path
-  end
-
-  private
-
-  def get_user_data(access_token)
-    conn = Faraday.new(
-      url: 'https://api.github.com',
-      headers: {
-        'Authorization': "token #{access_token}"
-      }
-    )
-
-    response = conn.get('/user')
-
-    JSON.parse(response.body, symbolize_names: true)
   end
 end
