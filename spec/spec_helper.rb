ceba04b5de485d767fd8eef0aef8ee02a3c49087
Curtis Bartell <cbartell980@gmail.com>
2021-01-02 17:39:27 -0700

    Add webmock requirement

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,7 @@ require 'simplecov'
 SimpleCov.start 'rails'
 
 require 'database_cleaner/active_record'
+require 'webmock/rspec'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
