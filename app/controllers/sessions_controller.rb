0c9b18434dab524cb0b703a4725c2424886f216c
Curtis Bartell <cbartell980@gmail.com>
2021-01-03 13:50:18 -0700

    Add GitHubFacade with find_or_create_user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,7 @@
 class SessionsController < ApplicationController
   def create
     access_token = OmniAuthFacade.access_token(params[:code])
-    user_data = GitHubService.user_data(access_token)
-    user = User.find_or_create_by(uid: user_data[:id])
-    user.username = user_data[:login]
-    user.uid = user_data[:id]
-    user.token = access_token
-    user.save
-
+    user = GitHubFacade.find_or_create_user(access_token)
     session[:user_id] = user.id
 
     redirect_to dashboard_path
